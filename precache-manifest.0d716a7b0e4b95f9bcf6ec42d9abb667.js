59f34735eef9ecde12d48b95efde6a17a113fb85
Jay Xiong (AU) <jay.xiong@global.ntt>
2020-04-22 09:50:34 +1000

    Updates

---
+++ precache-manifest.0d716a7b0e4b95f9bcf6ec42d9abb667.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc7c1133adb0a9dcfbe75805658fdac",
+    "url": "/tour-de-friends/index.html"
+  },
+  {
+    "revision": "5fb57a19f32e028b525e",
+    "url": "/tour-de-friends/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f59e889e348843fbe3c8",
+    "url": "/tour-de-friends/static/js/2.bdebe515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour-de-friends/static/js/2.bdebe515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb57a19f32e028b525e",
+    "url": "/tour-de-friends/static/js/main.cff362fa.chunk.js"
+  },
+  {
+    "revision": "83274b77684a0558c1ce",
+    "url": "/tour-de-friends/static/js/runtime-main.86cabf0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tour-de-friends/static/media/logo.5d5d9eef.svg"
+  }
+]);

5600f12c59b4ef583fa12f6b549c98e9bd6791bc
Jay Xiong (AU) <jay.xiong@global.ntt>
2020-04-23 19:32:53 +1000

    Updates

--- precache-manifest.8cb950d125a0decd117f652ae1f74548.js
+++ precache-manifest.8cb950d125a0decd117f652ae1f74548.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be8edcb6548fd6bbc612451c38efd084",
+    "revision": "e4b7d96c9eeaee85532899e6cccfd605",
     "url": "/tour-de-friends/index.html"
   },
   {
-    "revision": "db2a7d58699112db0679",
+    "revision": "f7f3e2d7d28f0a11682b",
     "url": "/tour-de-friends/static/css/main.a3950a90.chunk.css"
   },
   {
-    "revision": "cd480e2a4b1352e59b4f",
-    "url": "/tour-de-friends/static/js/2.988f2dd9.chunk.js"
+    "revision": "8f16fbf1a09ac9113f29",
+    "url": "/tour-de-friends/static/js/2.c74278c0.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/tour-de-friends/static/js/2.988f2dd9.chunk.js.LICENSE.txt"
+    "url": "/tour-de-friends/static/js/2.c74278c0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db2a7d58699112db0679",
-    "url": "/tour-de-friends/static/js/main.96b3ec7f.chunk.js"
+    "revision": "f7f3e2d7d28f0a11682b",
+    "url": "/tour-de-friends/static/js/main.d7f4b578.chunk.js"
   },
   {
     "revision": "83274b77684a0558c1ce",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tour-de-friends/precache-manifest.354f84cf783b30c51f5a10b64dc01c4f.js"
+  "/tour-de-friends/precache-manifest.8cb950d125a0decd117f652ae1f74548.js"
 );
 
 self.addEventListener('message', (event) => {
